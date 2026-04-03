dde54b563a2a81b0fa3ce9f4ce3630156b770507
Your Name <alex111222333grjc@gmail.com>
2026-04-04 02:25:20 +0300

    fix

--- tg/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-// middleware/auth.js
-module.exports = function requireAuth(req, res, next) {
-  if (!req.session.userId) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  next();
-};
--- tg/public/js/constants.js
+++
@@ -1,2 +0,0 @@
-// Константы приложения
-export const API_BASE_URL = '/api';
